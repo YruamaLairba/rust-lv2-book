96ac88d3fd463556821d619e450b13741c877f1f
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-02-02 22:39:22 +0100

    buildable skeleton implementation

--- metro/src/lib.rs
+++ metro/src/lib.rs
@@ -1,7 +1,39 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use lv2_atom::prelude::*;
+use lv2_core::prelude::*;
+use lv2_urid::prelude::*;
+
+#[derive(PortContainer)]
+pub struct Ports {
+    control: InputPort<AtomPort>,
+    output: OutputPort<Audio>,
+}
+
+#[derive(FeatureCollection)]
+pub struct Features<'a> {
+    map: Map<'a>,
+}
+
+pub struct Metro {
+    atom_urids: AtomURIDCache,
+}
+
+unsafe impl UriBound for Metro {
+    const URI: &'static [u8] = b"urn:rust-lv2-book:eg-metro-rs\0";
+}
+
+impl Plugin for Metro {
+    type Ports = Ports;
+
+    type Features = Features<'static>;
+
+    fn new(_plugin_info: &PluginInfo, features: Features<'static>) -> Option<Self> {
+        Some(Self {
+            atom_urids: features.map.populate_cache()?,
+        })
+    }
+
+    fn run(&mut self, ports: &mut Ports) {
     }
 }
+
+lv2_descriptors!(Metro);
