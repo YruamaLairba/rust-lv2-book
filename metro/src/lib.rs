bfe6ecfff3e9080fac4192b44d9eb6226b0ac0d9
YruamaLairba <Yruama_Lairba@hotmail.com>
2020-02-05 21:43:22 +0100

    construct many data

--- metro/src/lib.rs
+++ metro/src/lib.rs
@@ -2,6 +2,8 @@ use lv2_atom::prelude::*;
 use lv2_core::prelude::*;
 use lv2_units::prelude::*;
 use lv2_urid::prelude::*;
+use std::f64;
+use std::f64::consts::{PI};
 
 #[allow(unused_imports)]
 use lv2_sys::{
@@ -43,6 +45,15 @@ pub struct Features<'a> {
     map: Map<'a>,
 }
 
+const ATTACK_S:f64 = 0.005;
+const DECAY_S:f64 = 0.075;
+
+enum State {
+    Attack,
+    Decay,
+    Off,
+}
+
 pub struct Metro {
     atom_urids: AtomURIDCache,
     unit_urids: UnitURIDCache,
@@ -50,6 +61,21 @@ pub struct Metro {
     time_barBeat_urid: URID<TimeBarBeat>,
     time_beatPerMinute_urid: URID<TimeBeatPerMinute>,
     time_speed_urid: URID<TimeSpeed>,
+
+    rate: f64,  // Sample rate
+    bpm: f32 ,   // Beat per minute (tempo)
+    speed: f32, // Transport speed (usually 0=stop, 1=play)
+
+    elapsed_len: u32, // Frames since the start of the last click
+    wave_offset: u32, // Current play offset in the wave
+    state: State,     // current play state
+
+    // One cycle of a sine wave
+    wave: Vec<f32>,
+
+    // Envelope parameters
+    attack_len: u32,
+    decay_len: u32,
 }
 
 unsafe impl UriBound for Metro {
@@ -88,6 +114,18 @@ impl Plugin for Metro {
         _plugin_info: &PluginInfo,
         features: Features<'static>,
     ) -> Option<Self> {
+        
+        // Generate one cycle of a sine wave at the desired frequency
+        let rate: f64 = _plugin_info.sample_rate();
+        let freq: f64 = 440.0 * 2.0;
+        let amp: f64 = 0.5;
+        let wave_len: usize = (_plugin_info.sample_rate() / freq) as usize;
+        let mut wave: Vec<f32> = Vec::with_capacity(wave_len);
+        for i in 0..wave_len {
+            wave.push((f64::sin
+                    (i as f64 * 2f64 * PI * freq / rate) * amp) as f32);
+        }
+
         let res = Self {
             atom_urids: features.map.populate_cache()?,
             unit_urids: features.map.populate_cache()?,
@@ -95,6 +133,20 @@ impl Plugin for Metro {
             time_barBeat_urid: features.map.populate_cache()?,
             time_beatPerMinute_urid: features.map.populate_cache()?,
             time_speed_urid: features.map.populate_cache()?,
+
+            rate: _plugin_info.sample_rate(),
+            bpm: 120f32,
+            speed: 0f32,
+
+            elapsed_len: 0,
+            wave_offset: 0,
+            state: State::Off,
+
+            wave: wave,
+
+            attack_len: (ATTACK_S * _plugin_info.sample_rate()) as u32,
+            decay_len: (DECAY_S * _plugin_info.sample_rate()) as u32,
+
         };
         Some(res)
     }
