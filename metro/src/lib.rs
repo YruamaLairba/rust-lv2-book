90f7c439ff2f8a3994014f69c79e837edb935703
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2020-02-08 16:43:17 +0100

    Fixing the metronome to also listen for blank objects

--- metro/src/lib.rs
+++ metro/src/lib.rs
@@ -1,16 +1,17 @@
 use lv2_atom::prelude::*;
+use lv2_atom::space::*;
+use lv2_atom::Atom;
 use lv2_core::prelude::*;
 use lv2_units::prelude::*;
 use lv2_urid::prelude::*;
 use std::f64;
-use std::f64::consts::{PI};
+use std::f64::consts::PI;
 
 #[allow(unused_imports)]
 use lv2_sys::{
-    LV2_TIME__Position, LV2_TIME__Rate, LV2_TIME__Time, LV2_TIME__bar,
-    LV2_TIME__barBeat, LV2_TIME__beat, LV2_TIME__beatUnit,
-    LV2_TIME__beatsPerBar, LV2_TIME__beatsPerMinute, LV2_TIME__frame,
-    LV2_TIME__framesPerSecond, LV2_TIME__position, LV2_TIME__speed,
+    LV2_TIME__Position, LV2_TIME__Rate, LV2_TIME__Time, LV2_TIME__bar, LV2_TIME__barBeat,
+    LV2_TIME__beat, LV2_TIME__beatUnit, LV2_TIME__beatsPerBar, LV2_TIME__beatsPerMinute,
+    LV2_TIME__frame, LV2_TIME__framesPerSecond, LV2_TIME__position, LV2_TIME__speed,
     LV2_TIME_PREFIX, LV2_TIME_URI,
 };
 
@@ -34,6 +35,41 @@ unsafe impl UriBound for TimeSpeed {
     const URI: &'static [u8] = LV2_TIME__speed;
 }
 
+pub struct Blank;
+
+unsafe impl UriBound for Blank {
+    const URI: &'static [u8] = lv2_sys::LV2_ATOM__Blank;
+}
+
+impl URIDBound for Blank {
+    type CacheType = URID<Blank>;
+
+    fn urid(urid: &URID<Blank>) -> URID<Blank> {
+        *urid
+    }
+}
+
+impl<'a, 'b> Atom<'a, 'b> for Blank
+where
+    'a: 'b,
+{
+    type ReadParameter = <Object as Atom<'a, 'b>>::ReadParameter;
+    type ReadHandle = <Object as Atom<'a, 'b>>::ReadHandle;
+    type WriteParameter = <Object as Atom<'a, 'b>>::WriteParameter;
+    type WriteHandle = <Object as Atom<'a, 'b>>::WriteHandle;
+
+    fn read(body: Space<'a>, parameter: Self::ReadParameter) -> Option<Self::ReadHandle> {
+        Object::read(body, parameter)
+    }
+
+    fn init(
+        frame: FramedMutSpace<'a, 'b>,
+        parameter: Self::WriteParameter,
+    ) -> Option<Self::WriteHandle> {
+        Object::init(frame, parameter)
+    }
+}
+
 #[derive(PortContainer)]
 pub struct Ports {
     control: InputPort<AtomPort>,
@@ -45,8 +81,8 @@ pub struct Features<'a> {
     map: Map<'a>,
 }
 
-const ATTACK_S:f64 = 0.005;
-const DECAY_S:f64 = 0.075;
+const ATTACK_S: f64 = 0.005;
+const DECAY_S: f64 = 0.075;
 
 enum State {
     Attack,
@@ -57,18 +93,19 @@ enum State {
 pub struct Metro {
     atom_urids: AtomURIDCache,
     unit_urids: UnitURIDCache,
+    blank: URID<Blank>,
     time_position_urid: URID<TimePosition>,
     time_barBeat_urid: URID<TimeBarBeat>,
     time_beatPerMinute_urid: URID<TimeBeatPerMinute>,
     time_speed_urid: URID<TimeSpeed>,
 
     rate: f64,  // Sample rate
-    bpm: f32 ,   // Beat per minute (tempo)
+    bpm: f32,   // Beat per minute (tempo)
     speed: f32, // Transport speed (usually 0=stop, 1=play)
 
-    elapsed_len: u32, // Frames since the start of the last click
+    elapsed_len: u32,   // Frames since the start of the last click
     wave_offset: usize, // Current play offset in the wave
-    state: State,     // current play state
+    state: State,       // current play state
 
     // One cycle of a sine wave
     wave: Vec<f32>,
@@ -83,9 +120,8 @@ unsafe impl UriBound for Metro {
 }
 
 impl Metro {
-    fn play(&mut self, ports: &mut Ports, begin: usize, end: usize)
-    {
-        let frames_per_beat:f32 = 60f32 / self.bpm * self.rate as f32;
+    fn play(&mut self, ports: &mut Ports, begin: usize, end: usize) {
+        let frames_per_beat: f32 = 60f32 / self.bpm * self.rate as f32;
 
         if self.speed == 0f32 {
             ports.output.iter_mut().for_each(|x| *x = 0f32);
@@ -96,23 +132,22 @@ impl Metro {
             match self.state {
                 State::Attack => {
                     //Amplitude increase until attack_len
-                    ports.output[i] = 
-                        self.wave[self.wave_offset] *
-                        self.elapsed_len as f32 / self.attack_len as f32;
+                    ports.output[i] = self.wave[self.wave_offset] * self.elapsed_len as f32
+                        / self.attack_len as f32;
                     if self.elapsed_len >= self.attack_len {
                         self.state = State::Decay;
                     }
-                },
+                }
                 State::Decay => {
                     //Amplitude decrease until attack_len + decay_len
-                    ports.output[i] = 
-                        self.wave[self.wave_offset] *
-                        (1f32 - ((self.elapsed_len - self.attack_len)
-                                 as f32 / self.decay_len as f32));
+                    ports.output[i] = self.wave[self.wave_offset]
+                        * (1f32
+                            - ((self.elapsed_len - self.attack_len) as f32
+                                / self.decay_len as f32));
                     if self.elapsed_len >= self.attack_len + self.decay_len {
                         self.state = State::Off;
                     }
-                },
+                }
                 State::Off => {
                     ports.output[i] = 0f32;
                 }
@@ -121,7 +156,7 @@ impl Metro {
             self.wave_offset = (self.wave_offset + 1) % self.wave.len();
 
             //Update elapsed time and start attack if necessary
-            self.elapsed_len+=1;
+            self.elapsed_len += 1;
             if self.elapsed_len == frames_per_beat as u32 {
                 self.state = State::Attack;
                 self.elapsed_len = 0;
@@ -129,10 +164,7 @@ impl Metro {
         }
     }
 
-    fn update_position(
-        &mut self,
-        object_reader: lv2_atom::object::ObjectReader,
-    ) {
+    fn update_position(&mut self, object_reader: lv2_atom::object::ObjectReader) {
         //Received new transport position/speed
         for (property_header, atom) in object_reader {
             if property_header.key == self.time_beatPerMinute_urid {
@@ -150,16 +182,14 @@ impl Metro {
                     // Receveid a beat position, synchronise
                     // This hard sync may cause clicks, a real plugin would
                     // be more graceful
-                    let frames_per_beat =
-                        60f32 / self.bpm * self.rate as f32;
+                    let frames_per_beat = 60f32 / self.bpm * self.rate as f32;
                     let bar_beats = val;
                     let beat_beats = bar_beats - bar_beats.floor();
                     self.elapsed_len = (beat_beats * frames_per_beat) as u32;
                     if self.elapsed_len < self.attack_len {
                         self.state = State::Attack;
-                    } else if self.elapsed_len <
-                        self.attack_len + self.decay_len {
-                            self.state = State::Decay;
+                    } else if self.elapsed_len < self.attack_len + self.decay_len {
+                        self.state = State::Decay;
                     } else {
                         self.state = State::Off;
                     }
@@ -174,11 +204,7 @@ impl Plugin for Metro {
 
     type Features = Features<'static>;
 
-    fn new(
-        _plugin_info: &PluginInfo,
-        features: Features<'static>,
-    ) -> Option<Self> {
-        
+    fn new(_plugin_info: &PluginInfo, features: Features<'static>) -> Option<Self> {
         // Generate one cycle of a sine wave at the desired frequency
         let rate: f64 = _plugin_info.sample_rate();
         let freq: f64 = 440.0 * 2.0;
@@ -186,13 +212,13 @@ impl Plugin for Metro {
         let wave_len: usize = (_plugin_info.sample_rate() / freq) as usize;
         let mut wave: Vec<f32> = Vec::with_capacity(wave_len);
         for i in 0..wave_len {
-            wave.push((f64::sin
-                    (i as f64 * 2f64 * PI * freq / rate) * amp) as f32);
+            wave.push((f64::sin(i as f64 * 2f64 * PI * freq / rate) * amp) as f32);
         }
 
         let res = Self {
             atom_urids: features.map.populate_cache()?,
             unit_urids: features.map.populate_cache()?,
+            blank: features.map.map_type()?,
             time_position_urid: features.map.populate_cache()?,
             time_barBeat_urid: features.map.populate_cache()?,
             time_beatPerMinute_urid: features.map.populate_cache()?,
@@ -210,7 +236,6 @@ impl Plugin for Metro {
 
             attack_len: (ATTACK_S * _plugin_info.sample_rate()) as u32,
             decay_len: (DECAY_S * _plugin_info.sample_rate()) as u32,
-
         };
         Some(res)
     }
@@ -230,17 +255,21 @@ impl Plugin for Metro {
             let frames = timestamp.as_frames().unwrap() as usize;
             self.play(ports, last_t, frames);
 
-
-            if let Some((header, object_reader)) =
-                atom.read(self.atom_urids.object, ())
-            {
-                if header.otype == self.time_position_urid {
-                    self.update_position(object_reader);
-                }
-            }
-
             // Update time for next iteration
             last_t = frames;
+
+            let (header, reader) =
+                if let Some((header, reader)) = atom.read(self.atom_urids.object, ()) {
+                    (header, reader)
+                } else if let Some((header, reader)) = atom.read(self.blank, ()) {
+                    (header, reader)
+                } else {
+                    continue;
+                };
+
+            if header.otype == self.time_position_urid {
+                self.update_position(reader);
+            }
         }
 
         // Play for remainder of cycle
